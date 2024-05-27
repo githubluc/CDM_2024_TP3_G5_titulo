fbb68f33653089e982958c74ce998aa7167a88fb
lucas <lucasmartire@gmail.com>
2024-05-27 12:25:57 -0300

    Primer commit

---
+++ TP3/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3.c
+ *
+ * Created: 26/05/2024 4:49:17 pm
+ * Author : klee
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
